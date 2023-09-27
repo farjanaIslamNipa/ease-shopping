c205d4517e085a6b9663b37b994923565f6220fb
farjanaIslamNipa <farjana.jatri@gmail.com>
2023-09-27 16:46:50 +0600

    refactor code

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="products.length > 0">
+    <ProductsListItem v-for="product in products" :key="product.id" :product="product" />
+</div>
+<div v-else>
+    <p class="font-bold">You haven't added any item yet</p>
+</div>
+</template>
+
+<script>
+import ProductsListItem from './ProductsListItem.vue'
+    export default {
+        props: ['products'],
+        components: { ProductsListItem },
+        
+    }
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -1,18 +1,7 @@
 <template>
   <div id="page-wrap" class="bg-white rounded-2xl">
     <h1>Shopping Cart</h1>
-    <div
-      v-for="product in cartItems"
-      :key="product.id"
-      class="product-container"
-    >
-      <img class="product-image" :src="product.imageUrl" />
-      <div class="details-wrap">
-        <h3>{{ product.name }}</h3>
-        <p>${{ product.price }}</p>
-      </div>
-      <button class="remove-button">Remove From Cart</button>
-    </div>
+    <ProductsList :products="cartItems" />
     <h3 id="total-price">Total: ${{ totalPrice }}</h3>
     <button id="checkout-button">Proceed to Checkout</button>
   </div>
@@ -20,9 +9,11 @@
 
 <script>
 import { cartItems } from '../fake-data';
+import ProductsList from '../components/ProductsList.vue';
 
 export default {
     name: 'CartPage',
+    components: {ProductsList},
     data() {
       return {
         cartItems,
@@ -56,27 +47,5 @@ export default {
     width: 100%;
   }
 
-  .product-container {
-    align-content: 'center';
-    border-bottom: 1px solid #ddd;
-    display: flex;
-    padding: 16px;
-    width: 100%;
-  }
-
-  .product-image {
-    flex: 1;
-    height: 100px;
-    max-width: 100px;
-  }
-
-  .details-wrap {
-    padding: 0 16px;
-    flex: 3;
-  }
-
-  .remove-button {
-    flex: 1;
-    margin: auto;
-  }
+ 
 </style>
